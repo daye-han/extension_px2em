2df7976219c511d3226a75cca13335d87932067b
handaye <handot38@gmail.com>
2022-03-10 23:10:19 +0900

    init px2em

---
+++ extension.js
@@ -0,0 +1,51 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let cssPxToEm = vscode.commands.registerCommand('extension.px2em', function () {
+		cssPxToEmFunc();
+	});
+
+	context.subscriptions.push(cssPxToEm);
+}
+
+function cssPxToEmFunc() {
+	var editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+	// configuration schema - setting
+	const config = vscode.workspace.getConfiguration("px2em");
+	const decimalPoint = config.get('decimalPoint');
+	const unitCheck = config.get('unitCheck');
+
+	var selection = editor.selection;
+	var text = editor.document.getText(selection);
+
+	var _currentSize = Number(text.replace(/(\-?[0-9]*\.?[0-9]*)p?x?\/([0-9]*\.?[0-9]*)p?x?/ig, '$1'));
+	var _defaultSize = Number(text.replace(/(\-?[0-9]*\.?[0-9]*)p?x?\/([0-9]*\.?[0-9]*)p?x?/ig, '$2'));
+
+	var _calc = _currentSize / _defaultSize;
+	var _result = parseFloat(_calc.toFixed(decimalPoint));
+	unitCheck ? _result += 'em' : _result += '';
+
+	editor.edit((editBuilder) => {
+		editBuilder.replace(selection, _result);
+	});
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
